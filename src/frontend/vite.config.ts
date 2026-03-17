c2de3f4c1e52928a504b5c61ca9f3b663429c3f4
Madeleine.Von.Hausswolff.Extern@skanetrafiken.se <Madeleine.Von.Hausswolff.Extern@skanetrafiken.se>
2026-03-17 12:03:48 +0100

    fix comment

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '')
+  const env = loadEnv(mode, '.', 'VITE_')
 
   return {
     plugins: [react()],
