0a168ed026c7bf80d69b260a82cf4fb7891ed49b
Madeleine <madvonh@gmail.com>
2026-03-20 10:39:17 +0100

    deploy frontend

--- src/backend/RecipeCollection.AppHost/AppHost.cs
+++ src/backend/RecipeCollection.AppHost/AppHost.cs
@@ -2,9 +2,21 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.RecipeCollection_Api>("api")
     .WithHttpHealthCheck("/health");
-    
-    builder.AddViteApp(name: "recipecollection-frontend", appDirectory: "../../frontend" )
+
+if (builder.ExecutionContext.IsPublishMode)
+{
+    builder.AddDockerfile(name: "frontend", contextPath: "../../frontend")
+        .WithHttpEndpoint(targetPort: 80, name: "http")
+        .WithExternalHttpEndpoints()
         .WithReference(api)
         .WaitFor(api);
+}
+else
+{
+    builder.AddViteApp(name: "frontend", appDirectory: "../../frontend")
+        .WithExternalHttpEndpoints()
+        .WithReference(api)
+        .WaitFor(api);
+}
 
 builder.Build().Run();
